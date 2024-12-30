84088271fd91df7f5b092462ce6144b7505411a3
tomato.txt <104869883+ImMejor35@users.noreply.github.com>
2024-12-30 15:49:12 -0500

    Create APILoader.lua

---
+++ APILoader.lua
@@ -0,0 +1,9 @@
+getgenv().branch = "main"
+getgenv().owner = "ImMejor35"
+getgenv().git = function(path: string)
+    local result = "https://github.com/" .. owner .. "/BloxFruits/raw/refs/heads/" .. branch .. "/" .. path
+    return loadstring(game:HttpGet(result))()
+end
+
+getgenv().PlayerAPI = git("API/PlayerAPI.lua")
+getgenv().TeleportAPI = git("API/TeleportAPI.lua")
